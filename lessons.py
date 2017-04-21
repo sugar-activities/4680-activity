ef73be6bbbc846b066189270076978dd8123bb1e
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:43:36 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 3')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • The Light is the Source of everything that exists in this world.')) + \
+        '\n' + \
+        glib.markup_escape_text(_("   • The Light's nature is to always share.")) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • The Light is the Source of all spiritual fulfillment.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • The Light is always available to us.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Light: The Source of everything that exists in this world; a force of constant sharing that provides complete and unlimited spiritual fulfillment and happiness.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Describe a time when you felt the Light in your life – moments you felt happy, free or excited.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Describe a time when you did not feel the Light in your life – moments you felt sad, lonely or angry.')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 3')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • The Light is the Source of everything that exists in this world.')) + \
+        '\n' + \
+        glib.markup_escape_text(_("   • The Light's nature is to always share.")) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • The Light is the Source of all spiritual fulfillment.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • The Light is always available to us.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Light: The Source of everything that exists in this world; a force of constant sharing that provides complete and unlimited spiritual fulfillment and happiness.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Describe a time when you felt the Light in your life – moments you felt happy, free or excited.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Describe a time when you did not feel the Light in your life – moments you felt sad, lonely or angry.')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 4')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #3: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('We all have a spark of Light inside of us.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • We each have a spark of the Light within us.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Our essence is goodness.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Inside each one of us, there is the tremendous power of the Light.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Each one of us is special and unique.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+         glib.markup_escape_text(_('   • Think about a time when you were expressing a Light-quality. What happened? How did you feel?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Think about a time when you weren’t expressing your Light-qualities. What happened? How did you feel?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Do you always remember that everyone has Light inside of them? Why or why not?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 4')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #3: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('We all have a spark of Light inside of us.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • We each have a spark of the Light within us.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Our essence is goodness.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Inside each one of us, there is the tremendous power of the Light.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Each one of us is special and unique.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+         glib.markup_escape_text(_('   • Think about a time when you were expressing a Light-quality. What happened? How did you feel?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Think about a time when you weren’t expressing your Light-qualities. What happened? How did you feel?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Do you always remember that everyone has Light inside of them? Why or why not?')),
+)
---
+++ videos.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("3.1.ogv",
+     _("The Light"),
+     _("The Light is the Source of everything that exists in this world.\n\
+The Light's nature is to always share.\n\
+The Light is the Source of all spiritual fulfillment.\n\
+The Light is always available to us."),
+      ),
+    ("3.2.ogv",
+     _("The Unseen Power"),
+     _("The Light is the Source of everything that exists in this world.\n\
+The Light's nature is to always share.\n\
+The Light is the Source of all spiritual fulfillment.\n\
+The Light is always available to us."),
+      ),
+    ("4.ogv",
+     _('I am a Key Player'),
+     _('We each have a spark of the Light within us.\n\
+Our essence is goodness.\n\
+Inside each one of us, there is the tremendous power of the Light.\n\
+Each one of us is special and unique.'),
+     ),
+    ("4.1.ogv",
+     _('The One Thing'),
+     _('We each have a spark of the Light within us.\n\
+Our essence is goodness.\n\
+Inside each one of us, there is the tremendous power of the Light.\n\
+Each one of us is special and unique.'),
+     ),
+)
